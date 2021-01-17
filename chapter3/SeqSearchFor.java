adafa0ea5ecab51717b4718d25448c1da7e06391
홍진영 <jean@MacBook-Pro.local>
2021-01-17 15:40:58 +0900

    20210117 chapter3 SeqSearchSen

---
+++ chapter3/SeqSearchFor.java
@@ -0,0 +1,37 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class SeqSearchFor {
+
+    static int seqSearch(int[] a, int n, int key){
+        for(int i = 0; i < n ;i++){
+            if(a[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("num of elements");
+        int num = stdIn.nextInt();
+        int[] x = new int[num];
+
+        for(int i = 0; i < num; i++){
+            System.out.println("x["+i+"]");
+            x[i]=stdIn.nextInt();
+        }
+     System.out.println("value :");   
+     int ky = stdIn.nextInt();
+
+     int idx = seqSearch(x, num, ky);
+
+     if(idx == -1)
+            System.out.println("doesn't exist ");
+     else
+            System.out.println("exist : x["+idx+"]" );
+    }
+}
